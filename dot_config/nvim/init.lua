622141b18d0065a973e3fa41a382ee7fc4c205ca
AndrewCouncil <andrew.p.council@gmail.com>
2022-10-22 12:42:22 -0400

    neovim config done in lua

---
+++ dot_config/nvim/init.lua
@@ -0,0 +1,11 @@
+vim.opt.number=true
+vim.opt.relativenumber=true
+vim.opt.smartcase=true
+vim.opt.smartindent=true
+vim.opt.splitbelow=true
+vim.opt.splitright=true
+vim.opt.undofile=true
+vim.opt.expandtab=true
+vim.opt.shiftwidth=4
+vim.opt.tabstop=4
+
